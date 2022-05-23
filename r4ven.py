2123818a546650702da74cc6b10ba0a760867a18
Spyboy <itisshubhamkumar@gmail.com>
2022-05-24 01:48:17 +0530

    Update r4ven.py

--- r4ven.py
+++ r4ven.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
-VERSION = '1.1.0'
-
-R = '\033[31m'  # red
-G = '\033[32m'  # green
-C = '\033[36m'  # cyan
-W = '\033[0m'   # white
-Y = '\033[33m'  # yellow
-
 import sys
 import os
 import subprocess
@@ -16,7 +8,15 @@ discord = 'https://spyboy.in/Discord'
 website = 'https://spyboy.in/'
 blog = 'https://spyboy.blog/'
 
-art = r'''                                                    
+VERSION = '1.1.0'
+
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'   # white
+Y = '\033[33m'  # yellow
+
+banner = r'''                                                    
 __________    _________   _______________ _______   
 \______   \  /  |  \   \ /   /\_   _____/ \      \  
  |       _/ /   |  |\   Y   /  |    __)_  /   |   \ 
@@ -24,7 +24,7 @@ __________    _________   _______________ _______
  |____|_  /\____   |  \___/   /_______  /\____|__  /
         \/      |__|                  \/         \/ '''
 
-print(f'{R}{art}{W}\n')
+print(f'{R}{banner}{W}\n')
 print(f'{G}[+] {C}Version      : {W}{VERSION}')
 print(f'{G}[+] {C}Created By   : {W}Spyboy')
 print(f'{G} ╰➤ {C}Twitter      : {W}{twitter_url}')
