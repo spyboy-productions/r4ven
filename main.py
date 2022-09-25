913b4d7767df792193a62062b3933da2f63e290b
R3tr0bs <54243532+R3tr0bs@users.noreply.github.com>
2022-09-25 17:24:12 +0300

    Base done

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+#!/usr/bin/env python3
 """
 Author: R3tr0
 Date: 25/09/2022
-Purpose: 
+Purpose: will handle the fast api requests.
 """
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from utils import get_file_data
+
+FILE_NAME = "index_new.html"
+web_app = FastAPI()
+
+
+@web_app.get("/", response_class=HTMLResponse)
+def get_website():
+    """
+    gets the regular website for the victim device
+    """
+    html_data = ""
+    try:
+        html_data = get_file_data(FILE_NAME)
+    except FileNotFoundError:
+        pass
+    return html_data
+
+
+@web_app.post("/location_update")
+def update_location():
+    """
+    handles the location update of the program from the client side
+    """
+    pass
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 """
 Author: R3tr0
 Date: 25/09/2022
-Purpose: 
+Purpose: will hold the util functions of the program
 """
+
+
+def get_file_data(file_path):
+    """
+    gets the file data
+    :param file_path: the path to the file you want to read
+    :return: the file data as plain text
+    """
+    with open(file_path, 'r') as open_file:
+        return open_file.read()
